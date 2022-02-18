10cdfa3e46dc175fdf3f30f46b84de8b2fdf8754
Rasmus Bengtsson <rasmus@beryllium-tech.eu>
2022-02-18 11:56:35 +0100

    basic file structure and uml implementations

---
+++ src/main/java/data/GomokuGameState.java
@@ -0,0 +1,41 @@
+package data;
+
+import lab4.client.GomokuClient;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class GomokuGameState {
+    public GomokuGameState(GomokuClient gc) {
+    }
+
+    public String getMessageString() {
+        throw new NotImplementedException();
+    }
+
+    public GameGrid getGameGrid() {
+        throw new NotImplementedException();
+    }
+
+    public void move(int x, int y){
+        throw new NotImplementedException();
+    }
+
+    public void newGame() {
+        throw new NotImplementedException();
+    }
+
+    public void receivedNewGame() {
+        throw new NotImplementedException();
+    }
+
+    public void otherGuyLeft() {
+        throw new NotImplementedException();
+    }
+
+    public void disconnect() {
+        throw new NotImplementedException();
+    }
+
+    public void receivedMove(int x, int y) {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/main/java/data/TileState.java
@@ -0,0 +1,8 @@
+package data;
+
+//TODO: rename ME, OTHER, NONE
+public enum TileState {
+    ME,
+    OTHER,
+    NONE
+}
